a7f6f7b0089471390705022394817173ae06b4c0
Harikrishnakarna <131870223+Harikrishnakarna@users.noreply.github.com>
2024-01-02 12:26:05 +0530

    Add files via upload
    main project

---
+++ db_connection.php
@@ -0,0 +1,14 @@
+<?php
+
+// Database connection parameters
+$host = 'localhost';
+$dbname = 'gg_android_app';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error connecting to the database: " . $e->getMessage());
+}
